cf2fe4ec3e189411d4b15adb1d343b3edb6d995f
Truberton <gertandry@gmail.com>
2018-03-18 16:05:57 +0200

    Lisasin baasi

---
+++ FlashKaardiProgramm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashKaardiProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
